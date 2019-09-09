fbaa62e2663cc2a08e099b2c073bb2d15b4e08d0
exuberant-pool-9484 <alisherfayzimatov@gmail.com>
2019-09-09 00:33:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+function theBeatlesPlay(arg1,arg2){
+  var arg3 = []
+  for(var i = 0; i<arg1.length; i++){
+      arg3.push(arg1[i] + " plays " + arg2[i])
+     
+  }return arg3
+}
+function johnLennonFacts(arg){
+  var i = 0
+  var g=[]
+  while(i<arg.length){
+    g.push(arg[i]+ "!!!")
+    i++
+  }return g
+}
+function iLoveTheBeatles(int){
+  var ea = []
+  do{
+    ea.push("I love the Beatles!")
+    int++
+  }while(int<15)
+  return ea
+}
